d09a2b7f882ba7d33c1bafb7ea56b9fe499a1dd5
Pablo Ramos <pablo.ramos@iairgroup.com>
2024-10-25 09:33:34 +0200

    Modificar test

--- dbt_scripts/tests/generic/accepted_values_not_case_sensitive.sql
+++ dbt_scripts/tests/generic/accepted_values_not_case_sensitive.sql
@@ -1,4 +1,4 @@
-{% test accepted_values_not_case_sensitive(model, values, column_name) %}
+{% test accepted_values_not_case_sensitive(model, column_name, values) %}
 
     WITH accepted_values AS (
         SELECT {{ column_name }}
@@ -6,7 +6,6 @@
         WHERE LOWER({{ column_name }}::STRING) NOT IN (
             {% for value in values -%}
                 '{{ value|lower }}'
-                {%- if not loop.last -%},{%- endif %}
             {%- endfor %}
         )
     )
